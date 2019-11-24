8ba877a8db057d5ca8a319af3bc35427be2f3e97
Hubert Bereś <h.beres@hotmail.com>
2019-11-24 01:37:31 +0000

    The old example

--- cal_setup.py
+++ cal_setup.py
@@ -2,12 +2,13 @@ import datetime
 import os.path
 import pickle
 
+import google.oauth2.credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
+SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
 
 CREDENTIALS_FILE = "client_secret.json"
 
@@ -26,11 +27,24 @@ def get_calendar_service():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
-            creds = flow.run_local_server(port=3000)
+            flow.redirect_uri = "http://localhost:8080/"
+            creds = flow.run_local_server(port=8080)
 
         # Save the credentials for the next run
+        print("Credentials dict:", credentials_to_dict(creds))
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
     service = build("calendar", "v3", credentials=creds)
     return service
+
+
+def credentials_to_dict(credentials):
+    return {
+        "token": credentials.token,
+        "refresh_token": credentials.refresh_token,
+        "token_uri": credentials.token_uri,
+        "client_id": credentials.client_id,
+        "client_secret": credentials.client_secret,
+        "scopes": credentials.scopes,
+    }
